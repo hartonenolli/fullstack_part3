6c9efac9a5870b4e94b59213736df2daf298ccb0
hartonenolli <hartonenolli@gmail.com>
2025-07-31 11:16:21 +0300

    3.7: puhelinluettelon backend step7 and step8

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-
+const morgan = require('morgan')
+morgan('tiny')
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { id: 1, name: 'Arto Hellas', number: '040-1234567' },
